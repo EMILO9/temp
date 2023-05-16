5500dd32033d2a982117bc2e4113bbdc4f734e1d
unknown <emilberolsen@gmail.com>
2023-05-16 07:45:42 +0200

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <p class="greeting">{{ greeting }}</p>
+</template>
+
+<style lang="scss" scoped>
+.greeting {
+  color: $base-color;
+  font-weight: bold;
+}
+</style>
